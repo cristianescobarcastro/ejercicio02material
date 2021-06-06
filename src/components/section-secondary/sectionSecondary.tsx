0b49646504f6d3a12b38741caec244ccbb4b4fe4
Cristian Escobar Castro <cristian.escobar@hqb.cl>
2021-06-06 16:19:05 -0400

    agrega botón para validar contexto

--- src/components/section-secondary/sectionSecondary.tsx
+++ src/components/section-secondary/sectionSecondary.tsx
@@ -4,9 +4,34 @@ import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            '& > *': {
+                margin: theme.spacing(1),
+                width: '25ch',
+            },
+        },
+        button: {
+            margin: theme.spacing(1),
+        },
+    }),
+);
 
 const SectionSecondary = () => {
     const contadorContext = useContext(ContadorContext);
+ 
+    const handleSubmit = (e: any): void => {      
+        e.preventDefault();
+        alert('Contexto Contador Hijo: '+ contadorContext.counter +' Último email: '+ contadorContext.email ); 
+    };
+
+    const classes = useStyles();  
+
     return (
         <div>
             <Container maxWidth="xl">
@@ -17,26 +42,40 @@ const SectionSecondary = () => {
                     justify="center"
                     style={{ minHeight: '30vh' }}>
                     <Grid >
-                        <Box>
-                            <TextField
-                                id="contador"
-                                label="Contador envíos"
-                                InputProps={{
-                                    readOnly: true,
-                                }}
-                                value={contadorContext.counter}
-                            />
-                        </Box>
-                        <Box>
-                            <TextField
-                                id="detalle"
-                                label="Email último envío"
-                                InputProps={{
-                                    readOnly: true,
-                                }}
-                                value={contadorContext.email} 
-                            />
-                        </Box>
+                        <form
+                            id="contact"
+                            className={classes.root}
+                            noValidate={false}
+                            onSubmit={handleSubmit}
+                            autoComplete="off">
+                            <Box>
+                                <TextField
+                                    id="contador"
+                                    label="Contador envíos"
+                                    InputProps={{
+                                        readOnly: true,
+                                    }}                                   
+                                    value={contadorContext.counter}
+                                />
+                            </Box>
+                            <Box>
+                                <TextField
+                                    id="detalle"
+                                    label="Email último envío"
+                                    InputProps={{
+                                        readOnly: true,
+                                    }}
+                                    value={contadorContext.email}
+                                />
+                            </Box>
+                            <Box>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit">
+                                   Ver Contexto</Button>
+                            </Box>
+                        </form>
                     </Grid>
                 </Grid>
             </Container>
